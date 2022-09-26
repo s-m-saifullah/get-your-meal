742e025b46df1c996194e2db8daf0a92b12a0053
shaquille19 <shakil.m.19@gmailcom>
2022-09-26 22:26:42 +0600

    added tailwindcss daisyui and fontawesome to the app

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#108987",
+
+          secondary: "#e89878",
+
+          accent: "#0ef7a5",
+
+          neutral: "#271D2A",
+
+          "base-100": "#FAF9FB",
+
+          info: "#4591ED",
+
+          success: "#0E773D",
+
+          warning: "#9B6603",
+
+          error: "#F0463D",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
