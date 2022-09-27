0a5456280910ec8d4c8ff779d0366882c7294f65
shaquille19 <shakil.m.19@gmailcom>
2022-09-27 14:09:05 +0600

    added sticky positioning to navbar

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -3,7 +3,7 @@ import Cart from "../Cart/Cart";
 
 const Header = ({ cart }) => {
   return (
-    <div className="px-10 bg-cyan-700">
+    <div className="px-10 bg-cyan-700 sticky top-0 z-10">
       <div className="navbar text-white">
         <div className="flex-1">
           <a href="/" className="btn btn-ghost normal-case text-xl">
